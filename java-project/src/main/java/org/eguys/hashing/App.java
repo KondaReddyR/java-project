20ff1a51489b13fd69bcc212807bb0846c09891e
Konda Reddy <841750+KondaReddyR@users.noreply.github.com>
2019-09-23 21:46:59 -0500

    Update App.java

--- java-project/src/main/java/org/eguys/hashing/App.java
+++ java-project/src/main/java/org/eguys/hashing/App.java
@@ -1,4 +1,5 @@
 package org.eguys.hashing;
+import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  * Hello world!
@@ -8,6 +9,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length < 1) {
+            System.err.println("Please provide an input!");
+            System.exit(0);
+        }
+        System.out.println(sha256hex(args[0]));
+    }
+
+    public static String sha256hex(String input) {
+        return DigestUtils.sha256Hex(input);
     }
 }
